d2580ae550b83abd50603237f057c228d2b51f6c
udesh-udayakumar <124115752+udesh-udayakumar@users.noreply.github.com>
2023-09-19 14:28:28 +0530

    Updated Terraform files

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 resource "google_compute_network" "vpc_network" {
-  project = var.project_name
-  name    = var.vpc_name
+
+  name                    = var.vpc_name
+  auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "dev_subnet" {
-  project       = var.project_name
+
   name          = var.subnet
   ip_cidr_range = "10.4.0.0/16"
   region        = var.region
@@ -16,7 +17,7 @@ resource "google_compute_subnetwork" "dev_subnet" {
 }
 
 resource "google_compute_instance" "dev_vm" {
-  project      = var.project_name
+
   name         = var.vm_name
   machine_type = var.machine_type
   zone         = var.zone
@@ -28,11 +29,50 @@ resource "google_compute_instance" "dev_vm" {
   }
 
   network_interface {
-    network = google_compute_network.vpc_network.name
+    network    = google_compute_network.vpc_network.name
+    subnetwork = google_compute_subnetwork.dev_subnet.name
 
     access_config {
       // Ephemeral public IP
     }
   }
   depends_on = [google_compute_network.vpc_network]
+}
+
+resource "google_compute_global_address" "private_ip_address" {
+
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+
+  network                 = google_compute_network.vpc_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
+}
+
+resource "google_sql_database_instance" "instance" {
+
+  name             = "private-instance-${random_id.db_name_suffix.hex}"
+  region           = var.region
+  database_version = "MYSQL_5_7"
+
+  depends_on = [google_service_networking_connection.private_vpc_connection]
+
+  settings {
+    tier = "db-f1-micro"
+    ip_configuration {
+      ipv4_enabled                                  = false
+      private_network                               = google_compute_network.vpc_network.id
+      enable_private_path_for_google_cloud_services = true
+    }
+  }
 }
--- provider.tf
+++ provider.tf
@@ -1,2 +1,5 @@
 provider "google" {
+  project = var.project_name
+  region  = var.region
+  zone    = var.zone
 }
